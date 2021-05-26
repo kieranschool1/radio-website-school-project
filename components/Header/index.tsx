bfa6c8da967f5493903e2b0460a9524eb096abbc
Kieran <42723993+EnderDev@users.noreply.github.com>
2021-05-26 10:53:41 +0000

    ✨ Add files

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,13 +3,15 @@ import { SocialBar } from "../SocialBar"
 import { GlobalStyle } from "../style"
 import { HeaderMain, StyledHeader } from "./style"
 
+import * as logo from "../../assets/logo.svg"
+
 export const Header = () => {
     return (
         <StyledHeader>
             <GlobalStyle />
             <SocialBar />
             <HeaderMain>
-
+                <img src={logo}></img>
             </HeaderMain>
         </StyledHeader>
     )
