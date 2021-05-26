6edba2bb0b92a66c28bce4c06269a9d0644a620a
Kieran <42723993+EnderDev@users.noreply.github.com>
2021-05-26 10:50:04 +0000

    ✨ Add files

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,16 @@
 import React from "react"
 import { SocialBar } from "../SocialBar"
 import { GlobalStyle } from "../style"
-import { StyledHeader } from "./style"
+import { HeaderMain, StyledHeader } from "./style"
 
 export const Header = () => {
     return (
         <StyledHeader>
             <GlobalStyle />
             <SocialBar />
+            <HeaderMain>
+
+            </HeaderMain>
         </StyledHeader>
     )
 }
