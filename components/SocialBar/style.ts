17dca32307abfb76e361ccae8a297f456fcb3c1c
Kieran <42723993+EnderDev@users.noreply.github.com>
2021-05-26 10:37:16 +0000

    ✨ Add files

--- components/SocialBar/style.ts
+++ components/SocialBar/style.ts
@@ -1,5 +1,14 @@
+import styled from "styled-components";
+
 export const StyledSocialBar = styled.div`
     width: 100%;
     height: 28px;
-    padding: 0 
+    padding: 0px 16px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    & > i {
+        
+    }
 `;
