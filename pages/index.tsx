04de5d32d0fc4a1f326843811d2030109584eaaf
Kieran <42723993+EnderDev@users.noreply.github.com>
2021-05-26 11:36:05 +0000

    ✨ Add files

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ import { Header } from '../components/Header'
 const Home = () => {
   return (
     <>
-        <NextSeo
-      title={"Chorus Radio • Home"}
-    ></NextSeo>
+      <NextSeo
+        title={"Chorus Radio • Home"}
+      />
       <Header />
       <div>eafdmghfdkjghfdgkdjfshgdsflkjgdfg</div>
     </>
