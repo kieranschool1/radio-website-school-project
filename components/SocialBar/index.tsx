e44eb2dbe845a584d34ec50179578caa559cb8c8
Kieran <42723993+EnderDev@users.noreply.github.com>
2021-05-26 10:42:44 +0000

    ✨ Add files

--- components/SocialBar/index.tsx
+++ components/SocialBar/index.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { SocialIcon, StyledSocialBar } from "./style"
 
+import * as call from "../"
+
 export const SocialBar = () => {
     return (
         <StyledSocialBar>
-            <SocialIcon  />
+            <SocialIcon icon={} />
         </StyledSocialBar>
     )
 }
