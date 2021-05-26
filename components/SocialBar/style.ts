3b23e92f6a61c0b8a4e084249853359346a8dbde
Kieran <42723993+EnderDev@users.noreply.github.com>
2021-05-26 10:40:11 +0000

    ✨ Add files

--- components/SocialBar/style.ts
+++ components/SocialBar/style.ts
@@ -13,15 +13,12 @@ export const SocialIcon = styled.a`
     width: 16px;
     height: 16px;
     opacity: 0.5;
-    background-image: var(--social-icon-image);
     background-size: cover;
     background-repeat: no-repeat;
 
-    ${({ icon }: { icon: any
-        size?: number
-        lsp?: number
-        rsp?: number
-      }) => css`
+    ${({ icon }: { icon: any }) => css`
+        background-image: ${icon};
+    `};
 
     &:hover {
         opacity: 0.7;
