cf91c79caf34e89b5fd8b39eb6c4a488055b3ddf
Kieran <42723993+EnderDev@users.noreply.github.com>
2021-05-26 10:31:25 +0000

    ✨ Add files

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,6 +2,9 @@ const NotFound = () => {
     return (
         <div>
             <h1>404</h1>
+            <p>We were unable to find that page or resource. :(</p>
         </div>
     )
 }
+
+export default NotFound;
