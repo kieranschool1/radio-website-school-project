218f176f52ba430fcdc45730e84312adbb98d3fd
Kieran <42723993+EnderDev@users.noreply.github.com>
2021-05-26 11:28:25 +0000

    ✨ Add files

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,10 @@ import Head from 'next/head'
 import { Header } from '../components/Header'
 
 const Home = () => {
+  document.title = `Chorus Radio • Home`;
+
   return (
     <>
-      <title>Chorus Radio • Home</title>
       <Header />
       <div>eafdmghfdkjghfdgkdjfshgdsflkjgdfg</div>
     </>
