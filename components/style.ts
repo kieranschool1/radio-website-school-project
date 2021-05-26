e8a4958fbad53d490ffd9b1622fd00f17d3a65e0
Kieran <42723993+EnderDev@users.noreply.github.com>
2021-05-26 10:59:54 +0000

    ✨ Add files

--- components/style.ts
+++ components/style.ts
@@ -1,5 +1,26 @@
 import { createGlobalStyle } from "styled-components";
 
+const Fonts = `
+@font-face {
+  font-family: 'DM Serif Display';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url(https://fonts.gstatic.com/s/dmserifdisplay/v5/-nFnOHM81r4j6k0gjAW3mujVU2B2G_5x0ujy.woff2) format('woff2');
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+
+@font-face {
+  font-family: 'DM Serif Display';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url(https://fonts.gstatic.com/s/dmserifdisplay/v5/-nFnOHM81r4j6k0gjAW3mujVU2B2G_Bx0g.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+
+`;
+
 export const GlobalStyle = createGlobalStyle`
     html, body {
         margin: 0;
@@ -10,3 +31,4 @@ export const GlobalStyle = createGlobalStyle`
         --chorus-accent-color: #114ADD;
         --chorus-white: #FFFFFF;
     }
+`;
